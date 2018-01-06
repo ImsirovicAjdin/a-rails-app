40f3a1632442f9d5db0e9e61ad54cb560cb238b6
ajdin.imsirovic <ajdin.imsirovic@outlook.com>
2018-01-06 17:53:12 -0500

    Add migration up and down methods

--- db/migrate/20180106223651_create_users.rb
+++ db/migrate/20180106223651_create_users.rb
@@ -1,8 +1,41 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
+#  def change
+#    create_table :users do |t|
+#
+#     t.timestamps
+#    end
+#  end
+  
+  def up
     create_table :users do |t|
-
+      #t.column "first_name", :string
+      #t.string "first_name" -- short version
+      #Table column types:
+      #binary, boolean, date, datetime, decimal, float, integer (becomes INT), string (becomes VARCHAR), text, time
+      
+      #Table column options:
+      #:limit => size
+      #:default => value
+      #:null => true/false
+      #:precision => number
+      #:scale => number
+      
+      # you don't have to remember to add a column for ID, Rails will do it for us; you only need to specify when you DON'T wanna do it
+      # like this: create_table :users, :id => false do |t|
+      t.column "first_name", :string, :limit => 25
+      t.string "last_name", :limit => 50
+      t.string "email", :default => '', :null => false
+      t.string "password", :limit => 40
+     
+      #t.datetime "created_at"
+      #t.datetime "updated_at"
+      # shorthand for above:
       t.timestamps
     end
   end
+  
+  def down
+    drop_table :users
+  end
+  
 end
