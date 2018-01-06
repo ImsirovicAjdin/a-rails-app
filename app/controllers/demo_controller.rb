4a31ca6d821844b1c37e8646df7b6110233da861
ajdin.imsirovic <ajdin.imsirovic@outlook.com>
2018-01-06 06:01:54 -0500

    Rails routing experiments in demo controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,7 +1,17 @@
 class DemoController < ApplicationController
   def index
+    render('index') #redundant, comes with sensible defaults
   end
   
   def hello
+    render('hello') #redundant, comes with sensible defaults
+  end
+  
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+  end
+  
+  def lynda
+    redirect_to('http://lynda.com')
   end
 end
