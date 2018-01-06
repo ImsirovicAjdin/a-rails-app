8282a5afe125e654b698cd61b72981c47f220225
ajdin.imsirovic <ajdin.imsirovic@outlook.com>
2018-01-06 17:39:16 -0500

    Add a model (which includes a migration with change method)

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+# rails generate model User
+# model name is singular, table name is plural
+# contrast with:
+# rails generate migration MigrationName, like: rails generate migration DoNothingYet
+class User < ApplicationRecord
+end
